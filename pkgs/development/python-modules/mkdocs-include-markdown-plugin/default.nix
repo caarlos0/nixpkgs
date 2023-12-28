c4fbf118db47a40dfd7286ddc3f0a1c78423429f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-12-28 11:59:15 -0300

    mkdocs-include-markdown-plugin: init at 6.0.4

---
+++ pkgs/development/python-modules/mkdocs-include-markdown-plugin/default.nix
@@ -0,0 +1,29 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, hatchling
+}:
+
+buildPythonPackage rec {
+  pname = "mkdocs-include-markdown-plugin";
+  version = "6.0.4";
+
+  src = fetchFromGitHub {
+    owner = "mondeja";
+    repo = "mkdocs-include-markdown-plugin";
+    rev = "v${version}";
+    sha256 = "sha256-wHaDvF+QsEa3G5+q1ZUQQpVmwy+oRsSEq2qeJIJjFeY=";
+  };
+
+  pyproject = true;
+  doCheck = false;
+
+  nativeBuildInputs = [ hatchling ];
+
+  meta = with lib; {
+    description = "Mkdocs Markdown includer plugin.";
+    homepage = "https://github.com/mondeja/mkdocs-include-markdown-plugin";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ caarlos0 ];
+  };
+}
